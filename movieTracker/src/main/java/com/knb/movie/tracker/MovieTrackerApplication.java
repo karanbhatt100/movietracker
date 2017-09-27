a4362d01af5b8eaf5932e0e7afb8817abeb75f06
karanbhatt100 <karanbhatt100@gmail.com>
2017-09-27 23:26:40 +0530

    FirstCommit TESTING

---
+++ movieTracker/src/main/java/com/knb/movie/tracker/MovieTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.knb.movie.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTrackerApplication.class, args);
+	}
+}
